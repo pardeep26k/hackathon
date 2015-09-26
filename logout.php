a00992fde847fca0b9e6f640c1426f987350cc21
unknown <GUR38583@GURM38583.ad.aricent.com>
2015-09-26 16:16:25 +0530

    change

--- logout.php
+++ logout.php
@@ -1,4 +1,14 @@
 <?php
+
+# File :	logout.php
+# Description :file use to destroy session
+# Input	: Request Parameter
+# Output:NA
+# Author : pardeep  
+# Version : v1.0
+# Date :26-9-2015
+
+
 session_start();
 @session_destroy();
 ob_start();

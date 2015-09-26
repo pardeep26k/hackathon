5e62147f0414f6b4043a4ade407adf7babea20f5
unknown <GUR38583@GURM38583.ad.aricent.com>
2015-09-26 16:16:10 +0530

    change

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php
+# File :	Index.php
+# Description :file call after login or signup from FB 
+# Input	: Request Parameter
+# Output:NA
+# Author : pardeep  
+# Version : v1.0
+# Date :26-9-2015
+
+
 include('include/header.php');
 include('include/footer.php');
 ?>
